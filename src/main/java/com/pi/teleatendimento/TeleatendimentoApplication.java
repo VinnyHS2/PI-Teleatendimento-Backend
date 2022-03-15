8d369b6d35e48e2261c582f88dbd2be1883d60c7
Vinny <vinnyhsz1@gmail.com>
2022-03-15 15:16:57 -0300

    Vinny: first commit

---
+++ src/main/java/com/pi/teleatendimento/TeleatendimentoApplication.java
@@ -0,0 +1,13 @@
+package com.pi.teleatendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleatendimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleatendimentoApplication.class, args);
+	}
+
+}
