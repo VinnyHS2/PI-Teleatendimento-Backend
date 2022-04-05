4ee84f130957f747cd45e3864fcf46e0a256882c
Vinny <vinnyhsz1@gmail.com>
2022-04-05 15:27:04 -0300

    PI-13: criação do docker para rodar o bando de dados

---
+++ src/main/java/com/pi/teleatendimento/model/professor.java
@@ -0,0 +1,24 @@
+package com.pi.teleatendimento.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "professor")
+public class professor {
+
+	@Id
+	@GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+	@GeneratedValue(generator = "UUIDGenerator")
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+	
+	
+}
